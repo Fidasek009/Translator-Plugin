1e6d1ed9570bd2e58277d9c65c31881d0ee1a371
Fidasek009 <102549834+Fidasek009@users.noreply.github.com>
2023-01-22 22:52:30 +0100

    Add files via upload

---
+++ answers/extractor.py
@@ -0,0 +1,50 @@
+import json
+import re
+
+CLEANR = re.compile('<.*?>')
+
+def cleanhtml(raw_html):
+  cleantext = re.sub(CLEANR, '', raw_html)
+  return cleantext
+
+
+question = None
+answers = []
+answer = ""
+
+
+with open("output.json", "r") as f:
+    data = json.load(f)
+
+
+with open('load.html', 'r', encoding='utf-8') as f:
+    for line in f:
+        try:
+            if "<h3" in line and question is None:
+                question = cleanhtml(line.strip('\n'))
+            elif "color: #ff0000" in line and question is not None:
+                answers.append(cleanhtml(line.strip('\n')))
+            elif "<img" in line and "Refer to the exhibit." not in question:
+                print(f"ATTENTION NEEDED AT: {question}")
+                print("image question")
+                question = None
+                answers = []
+                answer = ""
+            elif "</ul>" in line and question is not None and len(answers) < 1:
+                print(f"ATTENTION NEEDED AT: {question}")
+                print("no answers")
+                question = None
+                answers = []
+                answer = ""
+            elif "</ul>" in line and question is not None:
+                for i in answers:
+                    answer += f"- {i}\n"
+                data[question] = answer
+                question = None
+                answers = []
+                answer = ""
+        except:
+            print(f"ATTENTION NEEDED")
+
+with open('output.json', 'w') as f:
+    f.write(json.dumps(data, indent=4, ensure_ascii=False))
